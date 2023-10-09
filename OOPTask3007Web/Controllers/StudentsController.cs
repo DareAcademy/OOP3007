e1a27541016fcd3d46ef6f514ad521c58977eb6e
Osama <osama@dareacademy.org>
2023-10-09 17:38:04 +0300

    add file

---
+++ OOPTask3007Web/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OOPTask3007Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("NewStudents");
+        }
+
+        public IActionResult SaveData()
+        {
+            // code save in Db
+
+            return View("NewStudents");
+        }
+    }
+}
